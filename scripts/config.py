0fc4ab3da20a240fff3ac38f0bde6b1bab4d4845
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-11-28 22:13:30 +0700

    Remove config.py

--- scripts/config.py
+++ scripts/config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     app_name: str = "Awesome API"
     admin_email: str
     items_per_user: int = 50
-    scaptcha_api_key="91408207650e8ee981b190012199db46" 
     
     model_config = SettingsConfigDict(env_file=".env")
     
